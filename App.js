faa5013c652911b4f2a945974e926c9648c4b0bb
misheck-sinks <msinlala2345@gmail.com>
2022-06-28 15:59:46 +0200

    init

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,imageBackgro } from 'react-native';
 
 export default function App() {
   return (
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'purple',
     alignItems: 'center',
     justifyContent: 'center',
   },
